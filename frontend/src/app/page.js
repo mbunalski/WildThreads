8beaf206a4c33cd7dd0ea36fe7cfb79da46b2e64
bun <mbunalski@gmail.com>
2025-04-15 15:26:58 -0400

    fixed mobile css finally

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,9 +7,15 @@ import {Display} from './components/Display.js';
 
 export default function Home() {
   return (
-    <div className="min-h-screen text-center flex justify-center flex-col items-center">
+    <div className="relative min-h-screen overflow-auto">
+      {/* Background layer */}
+      <div className="fixed inset-0 z-0 bg-[url('/wildflower-field.png')] bg-cover bg-center bg-no-repeat" />
+
+      {/* Scrollable content */}
+      <div className="relative z-10 min-h-screen">
         <Header />
         <Display />
+      </div>
     </div>
   );
 }
